eaaa3009d49a6e05b75460eec20e3408cb4dea04
samira-n <s.nabievaaa@gmail.com>
2022-05-08 19:32:39 +0300

    changes

---
+++ main.py
@@ -0,0 +1,75 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+from data import users, orders, offers
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JSON_AS_ASCII'] = False
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50))
+    age = db.Column(db.Integer)
+    email = db.Column(db.String(100))
+    role = db.Column(db.String(100))
+    phone = db.Column(db.String(100))
+
+
+class Order(db.Model):
+    __tablename__ = 'order'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    description = db.Column(db.String(255))
+    start_date = db.Column(db.Date)
+    end_date = db.Column(db.Date)
+    address = db.Column(db.String(100))
+    price = db.Column(db.Integer)
+    customer_id = db.Column(db.Integer)
+    executor_id = db.Column(db.Integer)
+
+
+class Offer(db.Model):
+    __tablename__ = 'offer'
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer)
+    executor_id = db.Column(db.Integer)
+
+
+def main():
+    db.create_all()
+    insert_data()
+
+
+def insert_data():
+    new_orders = []
+    for order in orders:
+        new_orders.append(
+            Order(
+                id=order['id'],
+                name=order['name'],
+                description=order['description'],
+                start_date=datetime.strptime(order['start_date'], '%m/%d/%Y'),
+                end_date=datetime.strptime(order['end_date'], '%m/%d/%Y'),
+                address=order['address'],
+                price=order['price'],
+                customer_id=order['customer_id'],
+                executor_id=order['executor_id']
+
+            )
+        )
+        with db.session.begin():
+            db. session.add_all(new_orders)
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+
